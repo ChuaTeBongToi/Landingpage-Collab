02a454e6ffc72442c94884b67a6df1208936f05d
ChuaTeBongToi <hbaoanh2k9@gmail.com>
2023-07-21 20:13:31 +0700

    chan le simulator

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import myNav from './myNav';
 import mySologan from './mySologan';
 import myCopyright from './myCopyright';
 import background from './Login - Step 1 edited.jpg'
+import { render } from '@testing-library/react';
 
-export default function App() {
-  return (
-    <div>
-      <img id='background' src={background}></img>
-      {myNav()}
-      {mySologan()}
-      {myCopyright()}
-    </div>
-  )
+export default function App({num}) {
+  if (num % 2 == 0) {
+    return(
+      <h1>Chẵn nha</h1>
+    )
+  }
+  else {
+    return(
+      <h1>Lẻ nhé</h1>
+    )
+  }
 }
