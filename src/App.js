f29684604d71cb293d57d2001a34f6bcdc79d804
ChuaTeBongToi <hbaoanh2k9@gmail.com>
2023-07-21 14:38:16 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import myNav from './myNav';
 import mySologan from './mySologan';
 import myCopyright from './myCopyright';
+import background from './Login - Step 1.png'
 
 export default function App() {
   return (
     <div>
+      <img id='background' src={background}></img>
       {myNav()}
       {mySologan()}
       {myCopyright()}
