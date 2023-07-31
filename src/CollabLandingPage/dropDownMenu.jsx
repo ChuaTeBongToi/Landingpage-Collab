1c3529d6ebd5784f7850431cf2bbf832abd311bb
ChuaTeBongToi <hbaoanh2k9@gmail.com>
2023-07-31 17:19:19 +0700

    depl;oy

--- src/CollabLandingPage/dropDownMenu.jsx
+++ src/CollabLandingPage/dropDownMenu.jsx
@@ -5,23 +5,65 @@ import camera from './Camera _.png'
 export default function dropDownMenuElement() {
     return (
         <div id={styles.dropdownmenu}>
-            <ul id={styles.dropdownlist}>
+            <ul className={styles.dropdownlist}>
                 <ul className={styles.dropdownitem}>
                     <h1>Features</h1>
                     <li>
-                        <img src={camera} />
-                        <h2>Collba Assistant</h2>
-                        <p>Smart automation system</p>
+                        <img src={camera} alt='img'/>
+                        <div>
+                            <h2>Collba Assistant</h2>
+                            <p>Smart automation system</p>
+                        </div>
                     </li>
                     <li>
-                        <img src={camera} />
-                        <h2>Collba Assistant</h2>
-                        <p>Smart automation system</p>
+                        <img src={camera} alt='img'/>
+                        <div>
+                            <h2>Advanced Importer</h2>
+                            <p>Work with various media formats</p>
+                        </div>
                     </li>
                     <li>
-                        <img src={camera} />
-                        <h2>Collba Assistant</h2>
-                        <p>Smart automation system</p>
+                        <img src={camera} alt='img'/>
+                        <div>
+                            <h2>Product Management</h2>
+                            <p>Workspace reimagined</p>
+                        </div>
+                    </li>
+                </ul>
+                <hr></hr>
+                <ul className={styles.dropdownitem}>
+                    <h1>Features</h1>
+                    <li>
+                        <img src={camera} alt='img'/>
+                        <div>
+                            <h2>Support</h2>
+                            <p>24/7 Live Support</p>
+                        </div>
+                    </li>
+                    <li>
+                        <img src={camera} alt='img'/>
+                        <div>
+                            <h2>Color Picker</h2>
+                            <p>Advanced color picker</p>
+                        </div>
+                    </li>
+                </ul>
+                <hr></hr>
+                <ul className={styles.dropdownitem}>
+                    <h1>Company</h1>
+                    <li>
+                        <img src={camera} alt='img'/>
+                        <div>
+                            <h2>Collab Team</h2>
+                            <p>See who’s behind the magic</p>
+                        </div>
+                    </li>
+                    <li>
+                        <img src={camera} alt='img'/>
+                        <div>
+                            <h2>Collab Design</h2>
+                            <p>What we’re working on</p>
+                        </div>
                     </li>
                 </ul>
             </ul>
